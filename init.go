520d89ea00afc02d8a403ab98a7a8bbd99b7a36f
Starz0r <starz0r@starz0r.com>
2019-11-28 13:08:38 -0600

    log to stdout instead
    Trying to figure out why Go can't do things the correct is costing me time, so instead I'll revisit it later when I have actual time to debug things.

--- init.go
+++ init.go
@@ -2,37 +2,14 @@ package logger
 
 import (
 	"os"
-	"runtime"
-	"time"
 
-	"github.com/natefinch/lumberjack"
 	"github.com/rs/zerolog"
 )
 
-var (
-	l      zerolog.Logger
-	appdir string
-	defdir string
-)
+var l zerolog.Logger
 
 func init() {
-	// Find the platform-specific directory
-	switch runtime.GOOS {
-	case "windows":
-		appdir = os.Getenv("APPDATA")
-		defdir = "\\default\\"
-	default:
-		appdir = "/var/log"
-		defdir = "/default/"
-	}
-
 	// Allocate a new logger
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
-	l = zerolog.New(&lumberjack.Logger{
-		Filename:   appdir + defdir + os.Args[0] + time.Now().UTC().String() + ".log",
-		MaxSize:    128,
-		MaxBackups: 3,
-		MaxAge:     1,
-		Compress:   true,
-	}).With().Timestamp().Logger()
+	l = zerolog.New(os.Stdout).With().Timestamp().Logger()
 }
